49fb732efb7e3ec21200d7d88fab00bb644c7079
Simon Guillet <simon.guillet@free.fr>
2023-05-04 11:53:30 +0200

    list movies

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,18 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import getMovies from '../../api/Movies';
 
+// Style imports
+import './Home.css';
 
 const HomePage = () => {
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
 
+    useEffect(() => {
+        getMovies().then((data) => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setMovies(data);
+            }
+        });
+    }, []);
+
+    if (error) {
+        return <div>There was an error: {error.message}</div>;
+    }
 
     return(
-        <div>
-            Home page
-            <button onClick={() => {
-                localStorage.removeItem('user');
-                localStorage.removeItem('token');
-                window.location.href = '/login';
-            }}>
-                Logout
-            </button>
+        <div className="home-page">
+            <header>
+                <div className="title">Home page</div>
+                <button onClick={() => {
+                    localStorage.removeItem('user');
+                    localStorage.removeItem('token');
+                    window.location.href = '/login';
+                }}>
+                    Logout
+                </button>
+            </header>
+            <main>
+                <div className="content">
+                    <div className="content-title">Content</div>
+
+                    <div className="list-movies">
+                        {movies["hydra:member"] && movies["hydra:member"].map((movie) => (
+                            <a href={"/movie/" + movie.id} key={movie.id}>
+                                <div className="movie-card" key={movie.id}>
+                                    <div className="movie-image">
+                                        <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt={movie.title} />
+                                    </div>
+                                    <div className="movie-title">{movie.title}</div>
+                                </div>
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
---
+++ src/api/Movies.jsx
@@ -0,0 +1,21 @@
+
+const getMovies = () => {
+  const API_KEY = localStorage.getItem('token');
+    return fetch('https://127.0.0.1:8000/api/movies', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        Authorization: `Bearer ${API_KEY}`,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        return data;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+};
+
+export default getMovies;
