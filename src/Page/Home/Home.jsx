8b0035feeb245a3800d0932a6c8fce5973e5b8aa
Simon Guillet <simon.guillet@free.fr>
2023-05-16 15:10:13 +0200

    view movies list

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,59 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import getMovies from '../../api/Movies';
+import React, { useState, useEffect } from "react"
+import getMovies from "../../api/Movies"
 
 // Style imports
-import './Home.css';
+import "./Home.css"
 
 const HomePage = () => {
-    const [movies, setMovies] = useState([]);
-    const [error, setError] = useState(null);
+	const [movies, setMovies] = useState([])
+	const [error, setError] = useState(null)
+	const [page, setPage] = useState(1)
 
-    useEffect(() => {
-        getMovies().then((data) => {
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setMovies(data);
-            }
-        });
-    }, []);
+	useEffect(() => {
+		getMovies(page).then((data) => {
+			if (data.error) {
+				setError(data.error)
+			} else {
+				setMovies(data)
+			}
+		})
+	}, [page])
 
-    if (error) {
-        return <div>There was an error: {error.message}</div>;
-    }
+	const next = () => {
+		if (movies["hydra:view"]["hydra:next"]) {
+			setPage(page + 1)
+		}
+	}
 
-    return(
-        <div className="home-page">
-            <header>
-                <div className="title">Home page</div>
-                <button onClick={() => {
-                    localStorage.removeItem('user');
-                    localStorage.removeItem('token');
-                    window.location.href = '/login';
-                }}>
-                    Logout
-                </button>
-            </header>
-            <main>
-                <div className="content">
-                    <div className="content-title">Content</div>
+	const previous = () => {
+		if (movies["hydra:view"]["hydra:previous"]) {
+			setPage(page - 1)
+		}
+	}
 
-                    <div className="list-movies">
-                        {movies["hydra:member"] && movies["hydra:member"].map((movie) => (
-                            <a href={"/movie/" + movie.id} key={movie.id}>
-                                <div className="movie-card" key={movie.id}>
-                                    <div className="movie-image">
-                                        <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt={movie.title} />
-                                    </div>
-                                    <div className="movie-title">{movie.title}</div>
-                                </div>
-                            </a>
-                        ))}
-                    </div>
-                </div>
-            </main>
-        </div>
-    )
+	if (error) {
+		return <div>There was an error: {error.message}</div>
+	}
+
+	return (
+		<div className="home-page">
+			<header>
+				<div className="title">Home page</div>
+				<button
+					onClick={() => {
+						localStorage.removeItem("user")
+						localStorage.removeItem("token")
+						window.location.href = "/login"
+					}}
+				>
+					Logout
+				</button>
+			</header>
+			<main>
+				<div className="content">
+					<div className="content-title">Content</div>
+					<div className="list-movies">
+						{movies["hydra:member"] &&
+							movies["hydra:member"].map((movie) => (
+								<a href={"/movie/" + movie.id} key={movie.id}>
+									<div className="movie-card" key={movie.id}>
+										<div className="movie-image">
+											<img
+												src={
+													"https://image.tmdb.org/t/p/w500" +
+													movie.poster_path
+												}
+												alt={movie.title}
+											/>
+										</div>
+										<div className="movie-title">
+											{movie.title}
+										</div>
+									</div>
+								</a>
+							))}
+					</div>
+					{console.log(movies)}
+					<button onClick={previous}>Previous</button>
+					<button onClick={next}>Next</button>
+				</div>
+			</main>
+		</div>
+	)
 }
 
-export default HomePage;
+export default HomePage
--- src/api/Movies.jsx
+++ src/api/Movies.jsx
@@ -1,21 +1,19 @@
+const getMovies = (page) => {
+	const API_KEY = localStorage.getItem("token")
+	return fetch("https://127.0.0.1:8000/api/movies?page=" + page, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		Authorization: `Bearer ${API_KEY}`,
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			return data
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
 
-const getMovies = () => {
-  const API_KEY = localStorage.getItem('token');
-    return fetch('https://127.0.0.1:8000/api/movies', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        Authorization: `Bearer ${API_KEY}`,
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        return data;
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-};
-
-export default getMovies;
+export default getMovies
